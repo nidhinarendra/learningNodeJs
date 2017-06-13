0dae634c145a10e04420ae57a4375a0a6b082fd5
Nidhi Narendra <nidhi.narendra1@gmail.com>
2017-06-13 14:35:37 -0700

    added a style sheet and used express.static to link the static files to js

--- templateEngine.js
+++ templateEngine.js
@@ -6,6 +6,15 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+/*The style sheet will not be linked or accessible by this code and hence
+we do not get any styles when we run this code.
+we need a middleware between the req and res to include such static files
+like css styling.*/
+
+//to fire the middleware express has some inbuilt functionality
+app.use('/assets', express.static('assets'));
+//This allows the styling to be available to all the pages
+
 //send can be used when we have to send a normal string to the web page.
 //sendFile needs to be used to send a HTML file
 app.get('/', function(req,res){
