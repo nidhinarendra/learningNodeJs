9ca29dec538c00299ae3fd884c5c2e2a0cdee25d
Nidhi Narendra <nidhi.narendra1@gmail.com>
2017-06-12 17:40:09 -0700

    Serving a html page with routing using express

--- tryingExpress.js
+++ tryingExpress.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 
+//send can be used when we have to send a normal string to the web page.
+//sendFile needs to be used to send a HTML file
 app.get('/', function(req,res){
-  res.send('This is the home page');
+  //res.send('This is the home page');
+  res.sendFile(__dirname + '/index.html');
 })
 
 app.get('/contact', function(req,res){
-  res.send('This is the contact page');
+  res.sendFile(__dirname + '/contact.html');
 })
 
+//id is a variable and can be replaced with anything.
 app.get('/profile/:id', function(req,res){
   res.send('The profile id you requested for is ' + req.params.id);
 })
